9c07da4a0a558b148b7f9d3fd131a973db488462
SleepyZzed <mcverdict@gmail.com>
2020-08-25 19:15:44 +0100

    added a method to ban using console

--- ThirstBotV2/botclient.cs
+++ ThirstBotV2/botclient.cs
@@ -111,6 +111,38 @@ namespace ThirstBotV2
                 
                 repeat = true;
             }
+            if(response == "ban")
+            {
+                Console.WriteLine("Who would you like to ban");
+                string un = Console.ReadLine().ToString();
+                SocketGuildUser user =  _client.GetGuild(595663383777247297UL).Users.FirstOrDefault(x => x.Username == un);
+                
+               // var user = _client.GetGuild(595663383777247297UL).GetUser(userToID);
+                
+                try{
+                Console.WriteLine("Is this who you wish to ban? " + user.Username + "#" + user.Discriminator );
+
+                string yorno = Console.ReadLine().ToString();
+                if(yorno =="yes")
+                {    
+               
+                    
+                    await user.SendMessageAsync("I ysmirr have personally banned you. Feel honoured");
+                    await user.BanAsync();
+                    Console.WriteLine(user.Username + " has been banned");
+                    repeat = true;
+                    }
+                }
+                catch(NullReferenceException ex)
+                {
+                    Console.WriteLine("no user found: " +ex);
+                    Console.WriteLine("Aborting");
+                    repeat = true;
+                }
+                    
+
+
+            }
                 }
 
 
--- ThirstBotV2/token.cs
+++ ThirstBotV2/token.cs
@@ -8,9 +8,9 @@ namespace ThirstBotV2
     {
         
 
-        public static string tkn = "NzIzNDEyOTg0MzY5Nzc0NjAy.XuxQ1w.JacZfPL4qaOkiFO7FecgxKel1YA";
+        public static string tkn = "";
 
-        public static string tkn1 = "NzI2Mjk3NjA3MDE3NTk1MTYy.XvbPWw.8m9hCDs5hAJPd6gZidcf-gW3D-k";
+        public static string tkn1 = "";
 
     }
 }
